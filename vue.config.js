32489da61de500181bbc4f95b39e543208135f14
zxygary <zxy@zxygary.com>
2022-03-05 11:28:25 +0800

    目录结构设置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer:{
+    host:'localhost',
+    port:8080,
+    proxy:{
+      '/api':{
+        target:'https://www.imooc.com',
+        changeOrigin:true,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    }
+  }
+}
