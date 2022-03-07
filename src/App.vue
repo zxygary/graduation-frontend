d50d5265d8fe691df96f20aec8a8c454727142f9
zxygary <zxy@zxygary.com>
2022-03-07 19:18:43 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import storage from './storage'
 export default {
   name: 'app',
   components: {
@@ -16,7 +17,11 @@ export default {
     }
   },
   mounted(){
-    
+    // storage.setItem('a',1);
+    // storage.setItem('user',{a:1});
+    // storage.setItem('abc',{a:1},'user');
+    // storage.clear('a');
+    storage.clear('a','user');
   }
 }
 </script>
