5e77e030ada7466f36bccc8f59e19d7989316ec6
zxygary <zxy@zxygary.com>
2022-03-09 11:10:29 +0800

    环境设置

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import App from './App.vue'
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import 'swiper/css/swiper.css'
+import env from './env'
 
-export default {
-  components: { Swiper, SwiperSlide }
-}
+// 根据前端的跨域方式做调整 /a/b
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+// 根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = '/#/login';
+  }else{
+    alert(res.msg);
+  }
+});
+
+// Vue.use(VueAxios.axios);
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
   devServer:{
     host:'localhost',
     port:8080,
+    proxy:{
+      '/api':{
+        target:'https://www.imooc.com',
+        changeOrigin:true,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    }
   }
 }
