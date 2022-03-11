e03dfa90b1f48a932c87708e743a1d5c0cf1236e
zxygary <zxy@zxygary.com>
2022-03-11 17:10:42 +0800

    Nav-Header组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -3,15 +3,16 @@
     <div class="nav-topbar">
       <div class="container">
         <div class="topbar-menu">
-          <a href="javascript:;">小米商城</a>
+          <a href="javascript:;">商城</a>
           <a href="javascript:;">MUI</a>
           <a href="javascript:;">云服务</a>
           <a href="javascript:;">协议规则</a>
         </div>
         <div class="topbar-user">
-          <a href="javascript:;">登录</a>
-          <a href="javascript:;">注册</a>
-          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          <a href="javascript:;" v-if="username">{{username}}</a>
+          <a href="javascript:;" v-if="!username" @click="login">登录</a>
+          <a href="javascript:;" v-if="username">我的订单</a>
+          <a href="javascript:;" class="my-cart" @click="goToCart"><span class="icon-cart"></span>购物车</a>
         </div>
       </div>
     </div>
@@ -22,16 +23,84 @@
         </div>
         <div class="header-menu">
           <div class="item-menu">
-            <span>小米手机</span>
-            <div class="children"></div>
+            <span>手机</span>
+            <div class="children">
+              <ul>
+                <li class="product" v-for="(item,index) in phoneList" :key="index">
+                  <a v-bind:href="'/#/product/'+item.id" target="_blank">
+                    <div class="pro-img">
+                      <img :src="item.mainImage" :alt="item.subtitle">
+                    </div>
+                    <div class="pro-name">{{item.name}}</div>
+                    <div class="pro-price">{{item.price | currency}}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
-            <span>RedMi红米</span>
-            <div class="children"></div>
+            <span>电子产品</span>
           </div>
           <div class="item-menu">
             <span>电视</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-1.jpg" alt="">
+                    </div>
+                    <div class="pro-name">壁画电视 65英寸</div>
+                    <div class="pro-price">6999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-2.jpg" alt="">
+                    </div>
+                    <div class="pro-name">全面屏电视E55A</div>
+                    <div class="pro-price">1999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-3.png" alt="">
+                    </div>
+                    <div class="pro-name">4A电视 32英寸</div>
+                    <div class="pro-price">699元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-4.jpg" alt="">
+                    </div>
+                    <div class="pro-name">4A电视 55英寸</div>
+                    <div class="pro-price">1799元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-5.jpg" alt="">
+                    </div>
+                    <div class="pro-name">4A电视 65英寸</div>
+                    <div class="pro-price">2699元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-6.png" alt="">
+                    </div>
+                    <div class="pro-name">查看全部</div>
+                    <div class="pro-price">查看全部</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div class="header-search">
@@ -46,12 +115,48 @@
 </template>
 <script>
   export default{
-    name:'nav-header'
+    name:'nav-header',
+    data(){
+      return {
+        username:'',
+        phoneList:[]
+      }
+    },
+    filters:{
+      currency(val){
+        if(!val)return '0.00';
+        return '￥' + val.toFixed(2) + '元';
+      }
+    },
+    mounted(){
+      this.getProductList();
+    },
+    methods:{
+      login(){
+        this.$router.push('/login');
+      },
+      getProductList(){
+        this.axios.get('/products',{
+          params:{
+            categoryId:'100012',
+            pageSize:6
+          }
+        }).then((res)=>{
+          if(res.list.length>=6){
+            this.phoneList = res.list.slice(0,6);
+          }
+        })
+      },
+      goToCart(){
+        this.$router.push('/cart');
+      }
+    }
   }
 </script>
 <style lang="scss">
   @import './../assets/scss/base.scss';
-  // @import './../assets/scss/mixin.scss';
+  @import './../assets/scss/mixin.scss';
+  @import './../assets/scss/config.scss';
   .header{
     .nav-topbar{
       height:39px;
@@ -59,7 +164,7 @@
       background-color:#333333;
       color:#B0B0B0;
       .container{
-        // @include flex();
+        @include flex();
         a{
           display:inline-block;
           color:#B0B0B0;
@@ -71,7 +176,7 @@
           text-align:center;
           color:#ffffff;
           .icon-cart{
-           background: url('/../public/imgs/icon-cart-checked.png');
+            @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');
             margin-right:4px;
           }
         }
@@ -79,8 +184,9 @@
     }
     .nav-header{
       .container{
+        position:relative;
         height:112px;
-        // @include flex();
+        @include flex();
         .header-logo{
           display:inline-block;
           width:55px;
@@ -92,12 +198,12 @@
             height:55px;
             &:before{
               content:' ';
-              background: url('/imgs/mi-logo.png');
+              @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
               transition:margin .2s;
             }
             &:after{
               content:' ';
-              background: url('/imgs/mi-home.png');
+              @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
             }
             &:hover:before{
               margin-left:-55px;
@@ -107,7 +213,7 @@
         }
         .header-menu{
           display:inline-block;
-          // width:200px;
+          width:643px;
           padding-left:209px;
           .item-menu{
             display:inline-block;
@@ -115,19 +221,95 @@
             font-weight:bold;
             font-size:16px;
             line-height:112px;
-            margin-right: 20px;
+            margin-right:20px;
             span{
               cursor:pointer;
             }
             &:hover{
-              
+              color:$colorA;
+              .children{
+                height:220px;
+                opacity:1;
+              }
+            }
+            .children{
+              position:absolute;
+              top:112px;
+              left:0;
+              width:1226px;
+              height:0;
+              opacity:0;
+              overflow:hidden;
+              border-top:1px solid #E5E5E5;
+              box-shadow:0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+              z-index: 10;
+              transition:all .5s;
+              .product{
+                position:relative;
+                float:left;
+                width:16.6%;
+                height:220px;
+                font-size:12px;
+                line-height:12px;
+                text-align: center;
+                a{
+                  display:inline-block;
+                }
+                img{
+                  width:auto;
+                  height:111px;
+                  margin-top:26px;
+                }
+                .pro-img{
+                  height:137px;
+                }
+                .pro-name{
+                  font-weight:bold;
+                  margin-top:19px;
+                  margin-bottom:8px;
+                  color:$colorB;
+                }
+                .pro-price{
+                  color:$colorA;
+                }
+                &:before{
+                  content:' ';
+                  position:absolute;
+                  top:28px;
+                  right:0;
+                  border-left:1px solid $colorF;
+                  height:100px;
+                  width:1px;
+                }
+                &:last-child:before{
+                  display:none;
+                }
+              }
             }
           }
         }
         .header-search{
-          
+          width:319px;
+          .wrapper{
+            height:50px;
+            border:1px solid #E0E0E0;
+            display:flex;
+            align-items:center;
+            input{
+              border:none;
+              box-sizing: border-box;
+              border-right:1px solid #E0E0E0;
+              width:264px;
+              height:50px;
+              padding-left:14px;
+            }
+            a{
+              @include bgImg(18px,18px,'/imgs/icon-search.png');
+              margin-left:17px;
+            }
+          }
         }
       }
     }
- }
+  }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     port:8080,
     proxy:{
       '/api':{
-        target:'https://www.imooc.com',
+        target:'https://mall-pre.springboot.cn',
         changeOrigin:true,
         pathRewrite:{
           '/api':''
