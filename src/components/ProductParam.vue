30331673b5e0427ca3d1e8a0affefd1c27ac646e
zxygary <zxy@zxygary.com>
2022-03-14 16:51:06 +0800

    产品站参数组件的实现

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-bar">
+    <div class="container">
+      <div class="pro-title">
+        小米12
+      </div>
+      <div class="pro-param">
+        <a href="javascript:;">概述</a><span>|</span>
+        <a href="javascript:;">参数</a><span>|</span>
+        <a href="javascript:;">用户评价</a>
+        <slot name="buy"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name:'nav-bar'
+  }
+</script>
+<style lang="scss">
+  @import './../assets/scss/config.scss';
+  @import './../assets/scss/mixin.scss';
+  .nav-bar{
+    height:70px;
+    line-height:70px;
+    border:1px solid $colorH;
+    .container{
+      @include flex();
+      .pro-title{
+        font-size:$fontH;
+        color:$colorB;
+        font-weight:bold;
+      }
+      .pro-param{
+        font-size:$fontJ;
+        span{
+          margin:0 10px;
+        }
+        a{
+          color:$colorC;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </product-param>
   </div>
 </template>
 <script>
+  import ProductParam from './../components/ProductParam'
   export default{
-    name:'product'
+    name:'product',
+    components:{
+      ProductParam
+    }
   }
 </script>
+<style lang="scss">
+  .product{
+    button{
+      margin-left:10px;
+    }
+  }
+</style>
