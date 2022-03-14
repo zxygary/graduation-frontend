266f3cf806fd6565d929217f034950dc3bfe8885
zxygary <zxy@zxygary.com>
2022-03-14 17:19:53 +0800

    slide动画效果的实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -39,11 +39,11 @@
       <div class="item-video">
         <h2>60帧超慢动作摄影<br/>慢慢回味每一瞬间的精彩</h2>
         <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
-        <div class="video-bg"></div>
+        <div class="video-bg" @click="showSlide='slideDown'"></div>
         <div class="video-box">
-          <div class="overlay"></div>
-          <div class="video">
-            <span class="icon-close"></span>
+          <div class="overlay" v-if="showSlide=='slideDown'"></div>
+          <div class="video" v-bind:class="showSlide">
+            <span class="icon-close" @click="showSlide='slideUp'"></span>
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
         </div>
@@ -63,6 +63,7 @@
     },
     data(){
       return {
+        showSlide:'',
         swiperOption:{
           autoplay:true,
           slidesPerView:3,
@@ -158,21 +159,48 @@
           cursor:pointer;
         }
         .video-box{
-          
           .overlay{
             @include position(fixed);
             background-color:#333333;
             opacity:.4;
             z-index:10;
           }
+          @keyframes slideDown{
+            from{
+              top:-50%;
+              opacity:0;
+            }
+            to{
+              top:50%;
+              opacity:1;
+            }
+          }
+          @keyframes slideUp{
+            from{
+              top:50%;
+              opacity:1;
+            }
+            to{
+              top:-50%;
+              opacity:0;
+            }
+          }
           .video{
             position:fixed;
-            top:50%;
+            top:-50%;
             left:50%;
             transform:translate(-50%,-50%);
             z-index:10;
             width:1000px;
             height:536px;
+            opacity:1;
+            &.slideDown{
+              animation:slideDown .6s linear;
+              top:50%;
+            }
+            &.slideUp{
+              animation:slideUp .6s linear;
+            }
             .icon-close{
               position:absolute;
               top:20px;
