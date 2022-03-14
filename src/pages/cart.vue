8ca2e6cdac674dd647fe31b77649115720958add
zxygary <zxy@zxygary.com>
2022-03-14 20:58:28 +0800

    完成购物车模块

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -45,7 +45,7 @@
           </div>
           <div class="total fr">
             合计：<span>{{cartTotalPrice}}</span>元
-            <a href="javascript:;" class="btn">去结算</a>
+            <a href="javascript:;" class="btn" @click="order">去结算</a>
           </div>
         </div>
       </div>
@@ -128,6 +128,15 @@
         this.allChecked = res.selectedAll;
         this.cartTotalPrice = res.cartTotalPrice;
         this.checkedNum = this.list.filter(item=>item.productSelected).length;
+      },
+      // 购物车下单
+      order(){
+        let isCheck = this.list.every(item=>!item.productSelected);
+        if(isCheck){
+          alert('请选择一件商品');
+        }else{
+          this.$router.push('/order/confirm');
+        }
       }
     }
   }
