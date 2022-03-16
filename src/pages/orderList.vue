d6907a1233914b1ed7208a3a3f686a4bc8f9f0ce
zxygary <zxy@zxygary.com>
2022-03-16 23:11:34 +0800

    保留分页器

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -47,7 +47,7 @@
             </div>
           </div>
           <el-pagination
-            v-if="false"
+            v-if="true"
             class="pagination"
             background
             layout="prev, pager, next"
@@ -56,13 +56,14 @@
             @current-change="handleChange"
             >
           </el-pagination>
-          <div class="load-more" v-if="showNextPage">
+          <div class="load-more" v-if="false">
               <el-button type="primary" :loading="loading" @click="loadMore">加载更多</el-button>
           </div>
           <div class="scroll-more"
             v-infinite-scroll="scrollMore"
-            infinite-scroll-disabled="busy"
+            infinite-scroll-disabled="true"
             infinite-scroll-distance="410"
+            v-if="false"
           >
             <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading">
           </div>
